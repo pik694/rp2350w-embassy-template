7608f1d3f9e58d3f049e53197de84bb3bc282770
Piotr Żelazko <piotr.zelazko@icloud.com>
2025-09-23 23:19:27 +0200

    feat: add initial flake config

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "rp2350w-embassy-template";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        devTools = [
+          rustToolchain
+
+          pkgs.cargo-sort
+          pkgs.cargo-machete
+        ];
+
+        buildInputs = [
+          pkgs.flip-link
+          pkgs.probe-rs-tools
+        ];
+
+        bake-cyw43 = pkgs.writeShellApplication {
+          name = "bake-cyw43";
+          runtimeInputs = [ ];
+
+          text = ''
+            # Find repository root
+            REPO_ROOT=$(git rev-parse --show-toplevel)
+
+            ${pkgs.probe-rs-tools}/bin/probe-rs download "$REPO_ROOT/assets/cyw43-firmware/43439A0.bin" \
+              --binary-format bin --chip RP235x --base-address 0x10100000 && \
+            ${pkgs.probe-rs-tools}/bin/probe-rs download "$REPO_ROOT/assets/cyw43-firmware/43439A0_clm.bin" \
+              --binary-format bin --chip RP235x --base-address 0x10140000
+          '';
+        };
+
+      in
+      {
+        packages = {
+          inherit bake-cyw43;
+        };
+
+        apps = {
+          bake-cyw43 = {
+            type = "app";
+            program = "${bake-cyw43}/bin/bake-cyw43";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            buildInputs
+            ++ devTools
+            ++ [
+              bake-cyw43
+            ];
+        };
+      }
+    );
+
+}
