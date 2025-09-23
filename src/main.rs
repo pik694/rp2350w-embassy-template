823825548e047c3b8d2d6a26c8b1c22c25f76f7d
Piotr Żelazko <piotr.zelazko@icloud.com>
2025-09-23 23:45:28 +0200

    feat: cross-compilation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::InterruptHandler;
+
+use {defmt_rtt as _, panic_probe as _};
+
+use defmt::info;
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Hello World");
 }
